f261ded47773c70b52a64c986ac0f434bd8ca986
priyanka shrimant mane <priyamane828@gmail.com>
2023-11-14 16:15:28 +0530

    sec

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Favourite from "./components/Favourite";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+let MainComponent= () => {
+  return (
+      <div>
+          <Provider store={store}>
+            <Navbar/>
+            <Outlet/>
+          </Provider>
+      </div>
+  )
+}
+const mainRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainComponent/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/favourites",
+        element: <Favourite/>
+      }
+    ]
+  }
+])
+let element = document.getElementById('root');
+let root = ReactDOM.createRoot(element);
+root.render(<RouterProvider router={mainRouter}/>);
 reportWebVitals();
